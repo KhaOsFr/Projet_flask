442a8e47c104ee5ca9b5218e09286b9e49a51d5d
Simon <simrob59127@live.fr>
2024-06-10 12:14:38 +0200

    Affichage sportifs par events

--- models.py
+++ models.py
@@ -15,3 +15,18 @@ class vue_event_disc_pays(db.Model):
 
     def __repr__(self):
         return f'{self.id_event} : {self.nom} : {self.description} : {self.date_debut} : {self.date_fin} : {self.discipline} : {self.pays} : {self.img_p} : {self.img_e}'
+
+
+class vue_sportif_disc_pays(db.Model):
+    id_sportif = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(30), nullable=False)
+    prenom = db.Column(db.String(30), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    pays = db.Column(db.String(30), nullable=False)
+    img_p = db.Column(db.String(30), nullable=False)
+    discipline = db.Column(db.String(30), nullable=False)
+    img_s = db.Column(db.String(30), nullable=False)
+    id_event = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'{self.id_sportif} : {self.nom} : {self.prenom} : {self.age} : {self.pays} : {self.img_p} : {self.discipline} : {self.img_s} : {self.id_event}'
--- routes.py
+++ routes.py
@@ -5,10 +5,19 @@ from . import app, models
 @app.route('/')
 @app.route("/accueil")
 def accueil():
-    return render_template('accueil.html', title='Bienvenue sur notre site')
+    liste_events = models.vue_event_disc_pays.query.all()
+    if liste_events:
+        liste_events[0].is_active = True
+    return render_template('accueil.html', title='Bienvenue sur notre site', liste_e=liste_events)
 
 
 @app.route('/tous_events')
 def allEvents():
     liste_events = models.vue_event_disc_pays.query.all()
     return render_template('tous_events.html', title='Nos évènements', liste_e=liste_events)
+
+
+@app.route('/sportifs/<int:id_event>')
+def sportifs_by_event(id_event):
+    sportifs = models.vue_sportif_disc_pays.query.filter_by(id_event = id_event).all()
+    return render_template('sportifs.html', title='Sportifs participant à {id_event}', sportifs=sportifs)
